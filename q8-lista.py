c857f09fd0812ed910eb5e35f34145d25b18580b
Rafaela dos Anjos <163443768+13rafaela@users.noreply.github.com>
2025-05-05 16:24:40 -0300

    Add files via upload

---
+++ q8-lista.py
@@ -0,0 +1,6 @@
+import math
+print('Este programa calcula o volume do cilindro')
+raio = int(input('Digite o raio:'))
+altura = int(input('Digite a altura:'))
+volume = math.pi*(raio**2)*altura
+print('O volume do cilindro é:' , volume)

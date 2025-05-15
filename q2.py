10dd7a18a861659bf450a98795f797633db5eccc
Rafaela dos Anjos <163443768+13rafaela@users.noreply.github.com>
2025-05-15 14:00:41 -0300

    Add files via upload

---
+++ q2.py
@@ -0,0 +1,12 @@
+mfd = 60
+nat = int(input('digite a nota da avaliação teórica:'))
+nap = int(input('digite a nota da avaliação pratica:'))
+soma = nat + nap
+if soma < 60:
+    print('reprovado')
+else:
+    print('aprovado')
+    
+    
+    
+    
